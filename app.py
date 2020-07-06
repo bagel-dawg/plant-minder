a9521c42bcb9f8d80286aa0ff93cb16473432911
Bagel <ryan@cs.odu.edu>
2020-07-06 01:33:57 +0100

    scipy requirement and data normalization

--- app.py
+++ app.py
@@ -7,13 +7,25 @@ from apscheduler.scheduler import Scheduler
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from scipy import stats
 import os
+import logging
+from logging import Formatter, FileHandler
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
 starting_date = date(2020, 7, 1)
 plant_name = "Aphrodite"
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+handler.setFormatter(Formatter(
+    '%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'
+))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
@@ -22,7 +34,7 @@ cron = Scheduler(daemon=True)
 # Explicitly kick off the background thread
 cron.start()
 
-@cron.interval_schedule(minutes=5)
+@cron.interval_schedule(seconds=5)
 def log_environment():
     csv_file_name = 'static/environment_history.csv'
 
@@ -60,6 +72,8 @@ def log_environment():
                          parse_dates=[0], dayfirst=True, 
                          names=['time','temperature','humidity'])
 
+    df = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
+
     fig, ax = plt.subplots()
     ax.plot(df.index, df.values)
     ax.set_xticks(df.index)
@@ -78,16 +92,20 @@ def index():
 
 @app.route('/environment_stats')
 def environment_stats():
-    temperature, humidity = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+    logger.info('DHT22: Temperature reading: {temp}, Humidity reading: {humid}'.format(temp=temperature, humid=humidity))
 
     if temperature > 100 or temperature is None:
         temperature = None
+        logger.info('Temperature reading was over 100 or was none, something is probably wrong...')
     else:
         temperature = (temperature * 9/5) + 32
         temperature = round(temperature,2)
 
     if humidity > 100 or humidity is None:
         humidity = None
+        logger.info('Temperature reading was over 100 or was none, something is probably wrong...')
     else:
         humidity = round(humidity, 2)
 
